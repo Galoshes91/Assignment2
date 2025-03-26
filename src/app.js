4843539b47046aafc2b9b0409a20216f3aecc302
URIAHLAPTOP\uriah <uriah.ginn@gmail.com>
2025-03-26 17:57:03 +1000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import airports from '../A2_Airports.json';
+import flights from '../A2_Flights.json';
+
+function parseData() {
+    console.log(airports);
+    console.log(flights);
+}
+
+parseData();
