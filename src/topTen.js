24b07bb3c0e4ea0923f809c129b205ac868dcdbd
Uriah <uriah.ginn@gmail.com>
2025-04-10 21:59:52 +1000

    clean up

--- src/topTen.js
+++ src/topTen.js
@@ -2,14 +2,14 @@ import { DataAnalyser } from "./DataAnalyser";
 
 const dataAnalyser = new DataAnalyser();
 
-function setAirportTableHeaders(tableElement) {
+function setTableHeaders(tableElement, thirdHeader) {
 	const trh = document.createElement("tr");
 	trh.classList.add("table-header");
 	const txtArr = [];
 
 	txtArr.push("Airport 1");
 	txtArr.push("Airport 2");
-	txtArr.push("Number of flights");
+	txtArr.push(thirdHeader);
 
 	txtArr.forEach((arrVal) => {
 		const cell = document.createElement("td");
@@ -21,26 +21,7 @@ function setAirportTableHeaders(tableElement) {
 	tableElement.appendChild(trh);
 }
 
-function setFlightTableHeaders(tableElement) {
-	const trh = document.createElement("tr");
-	trh.classList.add("table-header");
-	const txtArr = [];
-
-	txtArr.push("Airport 1");
-	txtArr.push("Airport 2");
-	txtArr.push("Distance");
-
-	txtArr.forEach((arrVal) => {
-		const cell = document.createElement("td");
-		const cellText = document.createTextNode(arrVal);
-		cell.appendChild(cellText);
-		trh.appendChild(cell);
-	});
-
-	tableElement.appendChild(trh);
-}
-
-function createAirportTable(arr) {
+function createTable(arr, thirdHeader) {
 	const divElement = document.getElementById("tableContainer");
 	divElement.innerHTML = "";
 	let table = document.createElement("table");
@@ -49,7 +30,7 @@ function createAirportTable(arr) {
 
 	table.setAttribute("id", "airportTable");
 
-	setAirportTableHeaders(table);
+	setTableHeaders(table, thirdHeader);
 	//#endregion
 
 	arr.forEach((airport) => {
@@ -58,38 +39,6 @@ function createAirportTable(arr) {
 		txtArr.push(airport.airportA);
 		txtArr.push(airport.airportB);
 		txtArr.push(airport.count);
-
-		txtArr.forEach((arrVal) => {
-			const cell = document.createElement("td");
-			const cellText = document.createTextNode(arrVal);
-			cell.appendChild(cellText);
-			tr.appendChild(cell);
-		});
-
-		table.appendChild(tr);
-	});
-
-	divElement.appendChild(table);
-}
-
-function createFlightTable(arr) {
-	const divElement = document.getElementById("tableContainer");
-	divElement.innerHTML = "";
-	let table = document.createElement("table");
-
-	//#region Header columns
-
-	table.setAttribute("id", "airportTable");
-
-	setFlightTableHeaders(table);
-	//#endregion
-
-	arr.forEach((airport) => {
-		const tr = document.createElement("tr");
-		const txtArr = [];
-		txtArr.push(airport.airportA);
-		txtArr.push(airport.airportB);
-		txtArr.push(airport.timeDiff);
 
 		txtArr.forEach((arrVal) => {
 			const cell = document.createElement("td");
@@ -117,17 +66,17 @@ function getTopFlightNum() {
 		});
 	});
 
-	createAirportTable(airportArr);
+	createTable(airportArr, "Number of flights");
 }
 
 function getTopTimeDiff() {
     const airports = dataAnalyser.getBiggestTimezoneDiff();
 
 	airports.forEach(ap => {
-		ap.timeDiff = `${ap.timeDiff.hrs} Hours, ${ap.timeDiff.mins} Minutes`
+		ap.count = `${ap.timeDiff.hrs} Hours, ${ap.timeDiff.mins} Minutes`
 	});
 
-	createFlightTable(airports);
+	createTable(airports, "Distance");
 }
 
 document.getElementById("flightNum").onclick = () => getTopFlightNum();
